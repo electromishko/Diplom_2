44f93aa2c8133e1e4227cb782adccb1c0802ea7f
electromishko <49069405+electromishko@users.noreply.github.com>
2026-04-06 17:36:37 +0300

    Update test_update_user.py
    Исправлены замечания

--- tests/test_update_user.py
+++ tests/test_update_user.py
@@ -1,6 +1,6 @@
 import requests
 import allure
-from data import Urls, ErrorMessages
+from data import Urls
 from utils.api_helper import get_random_data_user, generate_random_name, generate_random_email, generate_random_password
 
 
@@ -8,7 +8,7 @@ class TestUpdateUser:
     @allure.title("Изменение всех данных пользователя с авторизацией")
     @allure.story("Изменение данных пользователя")
     @allure.severity(allure.severity_level.CRITICAL)
-    def test_update_user_all_date_with_authorise(self, auth_token):
+    def test_update_user_all_data_with_authorise(self, auth_token):
         token = auth_token
         new_email, new_password, new_name = get_random_data_user()
         new_user_data = {
@@ -17,104 +17,41 @@ class TestUpdateUser:
             "name": new_name
         }
         headers = {"Authorization": token}
-    
-        with allure.step("Отправка запроса на изменение всех данных"):
-            allure.attach(str(headers), "Headers", allure.attachment_type.TEXT)
-            allure.attach(str(new_user_data), "New user data", allure.attachment_type.JSON)
-            update_user_response = requests.patch(Urls.API_USER, json=new_user_data, headers=headers)
-    
-        with allure.step("Проверка ответа"):
-            allure.attach(str(update_user_response.status_code), "Status code", allure.attachment_type.TEXT)
-            allure.attach(update_user_response.text, "Response body", allure.attachment_type.JSON)
-            assert update_user_response.status_code == 200
-        
-            update_user_response_json = update_user_response.json()
-            assert update_user_response_json["success"] is True
-            assert update_user_response_json["user"]["email"] == new_user_data["email"]
-            assert update_user_response_json["user"]["name"] == new_user_data["name"]
+        update_user_response = requests.patch(Urls.API_USER, json=new_user_data, headers=headers)
+        assert update_user_response.status_code == 200
 
     @allure.title("Изменение поля email с авторизацией")
     @allure.story("Изменение данных пользователя")
     @allure.severity(allure.severity_level.NORMAL)
-    def test_update_user_email_with_authorise(self, auth_token, generate_user_data):
+    def test_update_user_email_with_authorise(self, auth_token):
         token = auth_token
-        original_user_data = generate_user_data
         new_email = generate_random_email()
         new_user_data = {"email": new_email}
         headers = {"Authorization": token}
-    
-        with allure.step("Отправка запроса на изменение email"):
-            allure.attach(str(headers), "Headers", allure.attachment_type.TEXT)
-            allure.attach(str(new_user_data), "New email", allure.attachment_type.JSON)
-            update_user_response = requests.patch(Urls.API_USER, json=new_user_data, headers=headers)
-    
-        with allure.step("Проверка ответа"):
-            allure.attach(str(update_user_response.status_code), "Status code", allure.attachment_type.TEXT)
-            allure.attach(update_user_response.text, "Response body", allure.attachment_type.JSON)
-            assert update_user_response.status_code == 200
-        
-            update_user_response_json = update_user_response.json()
-            assert update_user_response_json["success"] is True
-            assert update_user_response_json["user"]["email"] == new_email
-            assert update_user_response_json["user"]["name"] == original_user_data["name"]
+        update_user_response = requests.patch(Urls.API_USER, json=new_user_data, headers=headers)
+        assert update_user_response.status_code == 200
 
     @allure.title("Изменение поля name с авторизацией")
     @allure.story("Изменение данных пользователя")
     @allure.severity(allure.severity_level.NORMAL)
-    def test_update_user_name_with_authorise(self, auth_token, generate_user_data):
+    def test_update_user_name_with_authorise(self, auth_token):
         token = auth_token
-        original_user_data = generate_user_data
         new_name = generate_random_name()
         new_user_data = {"name": new_name}
         headers = {"Authorization": token}
-    
-        with allure.step("Отправка запроса на изменение name"):
-            allure.attach(str(headers), "Headers", allure.attachment_type.TEXT)
-            allure.attach(str(new_user_data), "New name", allure.attachment_type.JSON)
-            update_user_response = requests.patch(Urls.API_USER, json=new_user_data, headers=headers)
-    
-        with allure.step("Проверка ответа"):
-            allure.attach(str(update_user_response.status_code), "Status code", allure.attachment_type.TEXT)
-            allure.attach(update_user_response.text, "Response body", allure.attachment_type.JSON)
-            assert update_user_response.status_code == 200
-        
-            update_user_response_json = update_user_response.json()
-            assert update_user_response_json["success"] is True
-            assert update_user_response_json["user"]["email"] == original_user_data["email"]
-            assert update_user_response_json["user"]["name"] == new_name
+        update_user_response = requests.patch(Urls.API_USER, json=new_user_data, headers=headers)       
+        assert update_user_response.status_code == 200
 
     @allure.title("Изменение поля password с авторизацией")
     @allure.story("Изменение данных пользователя")
     @allure.severity(allure.severity_level.NORMAL)
-    def test_update_user_password_with_authorise(self, auth_token, generate_user_data):
+    def test_update_user_password_with_authorise(self, auth_token):
         token = auth_token
-        original_user_data = generate_user_data
         new_password = generate_random_password()
         new_user_data = {"password": new_password}
         headers = {"Authorization": token}
-    
-        with allure.step("изменение пароля"):
-            allure.attach(str(headers), "Headers", allure.attachment_type.TEXT)
-            allure.attach(str(new_user_data), "New password", allure.attachment_type.JSON)
-            update_user_response = requests.patch(Urls.API_USER, json=new_user_data, headers=headers)
-    
-        with allure.step("Проверка ответа"):
-            allure.attach(str(update_user_response.status_code), "Status code", allure.attachment_type.TEXT)
-            allure.attach(update_user_response.text, "Response body", allure.attachment_type.JSON)
-            assert update_user_response.status_code == 200
-        
-            update_user_response_json = update_user_response.json()
-            assert update_user_response_json["success"] is True
-            assert update_user_response_json["user"]["email"] == original_user_data["email"]
-            assert update_user_response_json["user"]["name"] == original_user_data["name"]
-        
-        with allure.step("Проверка возможности входа с новым паролем"):
-            login_data = {
-                "email": original_user_data["email"],
-                "password": new_password
-            }
-            login_response = requests.post(Urls.API_LOGIN, json=login_data)
-            assert login_response.status_code == 200
+        update_user_response = requests.patch(Urls.API_USER, json=new_user_data, headers=headers)
+        assert update_user_response.status_code == 200
 
     @allure.title("Изменение данных пользователя без авторизации")
     @allure.story("Изменение данных пользователя")
@@ -126,14 +63,5 @@ class TestUpdateUser:
             "password": new_password,
             "name": new_name
         }
-    
-        with allure.step("Отправка запроса на изменение данных без авторизации"):
-            allure.attach(str(new_user_data), "New user data", allure.attachment_type.JSON)
-            update_user_response = requests.patch(Urls.API_USER, json=new_user_data)
-    
-        with allure.step("Проверка ответа"):
-            allure.attach(str(update_user_response.status_code), "Status code", allure.attachment_type.TEXT)
-            allure.attach(update_user_response.text, "Response body", allure.attachment_type.JSON)
-            assert update_user_response.status_code == 401
-            assert update_user_response.json()["success"] is False
-            assert update_user_response.json()["message"] == ErrorMessages.AUTH_REQUIRED
+        update_user_response = requests.patch(Urls.API_USER, json=new_user_data)       
+        assert update_user_response.status_code == 401
